a543cabded188dc957cba8eee8fc88afb5c9b625
woeishi <woei@refect.net>
2015-07-29 17:20:01 +0200

    refactored PinDefault to helper class

--- nodes/JoinSplitStructNode.cs
+++ nodes/JoinSplitStructNode.cs
@@ -102,7 +102,7 @@ namespace VVVV.Struct
 					if (FIsJoin)
 					{
 						attr = new InputAttribute(entry.Key);
-						attr = TrySetDefault(attr as InputAttribute, entry.Value, definition.Defaults[entry.Key]);
+						attr = StructUtils.TrySetDefault(attr as InputAttribute, entry.Value, definition.Defaults[entry.Key]);
 					}
 					else
 						attr = new OutputAttribute(entry.Key);
@@ -126,56 +126,6 @@ namespace VVVV.Struct
 			
 			FPins = pins;
 			return s;
-		}
-		
-		
-		private InputAttribute TrySetDefault(InputAttribute attr, Type type, string defaultString)
-		{
-			if (!string.IsNullOrEmpty(defaultString))
-			{
-				double doubleDefault = 0;
-				switch(type.ToString())
-				{
-					case "System.Boolean":
-						bool boolDefault = false;
-						if (bool.TryParse(defaultString,out boolDefault))
-							attr.DefaultBoolean = boolDefault;
-						break;
-					case "System.Double":
-					case "System.Single":
-					case "System.Int32":
-						if (double.TryParse(defaultString, out doubleDefault))
-							attr.DefaultValue = doubleDefault;
-						break;
-					case "System.String":
-						attr.DefaultString = defaultString;
-						break;
-					case "VVVV.Utils.VMath.Vector2D":
-					case "VVVV.Utils.VMath.Vector3D":
-					case "VVVV.Utils.VMath.Vector4D":
-						var vectorString = defaultString.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-						attr.DefaultValues = new double[vectorString.Length];
-						for(int i=0; i<vectorString.Length; i++)
-						{
-							if (double.TryParse(vectorString[i].Trim(), out doubleDefault))
-								attr.DefaultValues[i] = doubleDefault;
-
-						}
-						break;
-					case "VVVV.Utils.VColor.RGBAColor":
-						var colorString = defaultString.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-						attr.DefaultColor = new double[colorString.Length];
-						for(int i=0; i<colorString.Length; i++)
-						{
-							if (double.TryParse(colorString[i].Trim(), out doubleDefault))
-								attr.DefaultColor[i] = doubleDefault;
-
-						}
-						break;
-						
-				}
-			}
-			return attr;
 		}
 		
 		/// <summary>
