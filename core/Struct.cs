93af372d2caf6b3ed759a08e85e1dd5e1704ef68
woeishi <woei@refect.net>
2015-12-17 19:35:30 +0100

    refactored helper methods to utils class

--- core/Struct.cs
+++ core/Struct.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using VVVV.PluginInterfaces.V2;
-using VVVV.PluginInterfaces.V2.NonGeneric;
 
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
@@ -9,7 +7,7 @@ using System.Xml.Serialization;
 namespace VVVV.Struct
 {
     [DataContract]
-     public class Property
+    public class Property
     {
         [DataMember, XmlAttribute]
         public string Name;
@@ -95,70 +93,5 @@ namespace VVVV.Struct
 			key = name;
 			data = new Dictionary<string,object>();
 		}
-		
-		public Struct DeepCopy()
-		{
-			if (this == null)
-				return null;
-			else
-			{
-				var s = new Struct(this.Key);
-				foreach (var entry in this.Data)
-					s.Data[entry.Key] = (entry.Value as ISpread).Clone();
-				return s;
-			}
-		}
 	}
-
-    public static class StructUtils
-    {
-        public static IOAttribute TrySetDefault(IOAttribute attr, Type type, string defaultString)
-        {
-            if (!string.IsNullOrEmpty(defaultString))
-            {
-                double doubleDefault = 0;
-                switch (type.ToString())
-                {
-                    case "System.Boolean":
-                        bool boolDefault = false;
-                        if (bool.TryParse(defaultString, out boolDefault))
-                            attr.DefaultBoolean = boolDefault;
-                        break;
-                    case "System.Double":
-                    case "System.Single":
-                    case "System.Int32":
-                        if (double.TryParse(defaultString, out doubleDefault))
-                            attr.DefaultValue = doubleDefault;
-                        break;
-                    case "System.String":
-                        attr.DefaultString = defaultString;
-                        break;
-                    case "VVVV.Utils.VMath.Vector2D":
-                    case "VVVV.Utils.VMath.Vector3D":
-                    case "VVVV.Utils.VMath.Vector4D":
-                        var vectorString = defaultString.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                        attr.DefaultValues = new double[vectorString.Length];
-                        for (int i = 0; i < vectorString.Length; i++)
-                        {
-                            if (double.TryParse(vectorString[i].Trim(), out doubleDefault))
-                                attr.DefaultValues[i] = doubleDefault;
-
-                        }
-                        break;
-                    case "VVVV.Utils.VColor.RGBAColor":
-                        var colorString = defaultString.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                        attr.DefaultColor = new double[colorString.Length];
-                        for (int i = 0; i < colorString.Length; i++)
-                        {
-                            if (double.TryParse(colorString[i].Trim(), out doubleDefault))
-                                attr.DefaultColor[i] = doubleDefault;
-
-                        }
-                        break;
-
-                }
-            }
-            return attr;
-        }
-    }
 }
